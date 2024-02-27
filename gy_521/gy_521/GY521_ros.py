130b48e66c220f8c81d3e260f2bd80ddcb78aa44
panitan103 <pantian103_54@yahoo.com>
2024-02-27 15:28:40 +0700

    Refactor GY87_Publisher to GY521_Publisher

--- gy_521/gy_521/GY521_ros.py
+++ gy_521/gy_521/GY521_ros.py
@@ -1,38 +1,29 @@
 import rclpy
 from rclpy.node import Node
 from sensor_msgs.msg import Imu
-from sensor_msgs.msg import MagneticField
-from rclpy.clock import ROSClock
-
-
-
 
 from .gy_521_module.GY521 import GY521_module
 
-class GY87_Publisher(Node):
+class GY521_Publisher(Node):
 
     def __init__(self):
         super().__init__('minimal_publisher')
         self.publisher_imu = self.create_publisher(Imu,'/imu_data', 10)
-        self.publisher_mag = self.create_publisher(Imu,'/mag_data', 10)
-        self.ros_clock = ROSClock()
+
         self.imu_msg = Imu()
-        self.mag_msg=MagneticField()
 
         timer_period = 0  # seconds
-        self.gy87 = GY521_module()
+        self.gy521 = GY521_module()
         self.timer = self.create_timer(timer_period, self.timer_callback)
 
     def timer_callback(self):
          # Read data from GY87.py
         # Replace the following lines with the appropriate code to read data from GY87.py
 
-        [accel, gyro, temp, mag, pressure]=self.gy87.get_all_data()
+        [accel, gyro, temp]=self.gy87.get_all_data()
         print(f"Acceleration: X={accel['x']:.2f}g, Y={accel['y']:.2f}g, Z={accel['z']:.2f}g")
-        print(f"Rotation: X={gyro['x']:.2f}°/s, Y={gyro['y']:.2f}°/s, Z={gyro['z']:.2f}°/s")
+        print(f"Rotation: X={gyro['x']:.2f}rad/s, Y={gyro['y']:.2f}rad/s, Z={gyro['z']:.2f}rad/s")
         print(f"Temperature ={temp:.2f}°C")
-        print(f"Magnetrometer: X={mag['x']:.2f}µT, Y={mag['y']:.2f}µT, Z={mag['z']:.2f}µT")
-        print(f"Pressure= {pressure:.2f}hPa")
         print("")
 
         self.imu_msg.header.stamp = self.get_clock().now().to_msg()
@@ -43,18 +34,12 @@ class GY87_Publisher(Node):
         self.imu_msg.angular_velocity.y = gyro['y']
         self.imu_msg.angular_velocity.z = gyro['z']
 
-        self.mag_msg.header.stamp = self.get_clock().now().to_msg()
-        self.mag_msg.magnetic_field.x = mag['x']
-        self.mag_msg.magnetic_field.y = mag['y']
-        self.mag_msg.magnetic_field.z = mag['z']
-
         self.publisher_imu.publish(self.imu_msg)
-        self.publisher_mag.publish(self.imu_msg)
 
 def main(args=None):
     rclpy.init(args=args)
 
-    gy521_publisher = GY87_Publisher()
+    gy521_publisher = GY521_Publisher()
 
     rclpy.spin(gy521_publisher)
 
